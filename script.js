56f7a3dcfdf23bde59fb4983918321f584d24d71
Britteany <miszbritteany@gmail.com>
2023-12-03 00:31:50 -0500

    Updated images and have the ability to switch them

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-const displayApi = document.querySelector(".api-data");
-const searchButton = document.querySelector(".fa-solid fa-magnifying-glass fa-lg")
+const container = document.querySelector(".api-data");
+const searchButton = document.querySelector(".search button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
 
-async function getWeather() {
-    const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=London&appid=b076a8a09b7ac1a9ea89b6d5d522bcb3",
-      { mode: "cors" }
-    );
-    const weatherData = await response.json();
-    console.log(weatherData);
-  };
-  getWeather();
+searchButton.addEventListener("click", () => {
+  const key = "b076a8a09b7ac1a9ea89b6d5d522bcb3";
+  const city = document.querySelector(".city").value;
 
-searchButton.addEventListener("click", () =>{
-    const key = "b076a8a09b7ac1a9ea89b6d5d522bcb3";
-    const searchCity = document.querySelector(".city").value;
+  if (city == "") return;
 
-    if(searchCity == '')
-    return;
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      const image = document.querySelector(".weather-box img");
+      const temperature = document.querySelector("weather-box .temperature");
+      const description = document.querySelector("weather-box .description");
+      const feels = document.querySelector("weather-details .feels span");
+      const wind = document.querySelector("weather-details .wind span");
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${key}`).then(response => response.json()).then(json => {
+      switch (json.weather[0].main) {
+        case "Sunny":
+          image.src = "sunny.jpg";
+          break;
 
+        case "Rainy":
+          image.src = "rainy.jpg";
+          break;
+
+        case "Snowy":
+          image.src = "snowy.jpg";
+          break;
+
+        case "Cloudy":
+          image.src = "cloud.jpg";
+          break;
+
+        case "Foggy":
+          image.src = "foggy.png";
+          break;
+
+        default:
+          image.src = "sunn.jpg";
+          break;
+      }
     });
-})
-  
+});
